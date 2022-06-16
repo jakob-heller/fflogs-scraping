6ce2af354b593e970ec6fd5f9c2f77ea90865839
jakob-heller <j4kob.heller@gmail.com>
2022-06-16 09:10:15 +0200

    added more links for testing

--- fflogs-study-project/input/user_input.py
+++ fflogs-study-project/input/user_input.py
@@ -7,8 +7,30 @@ def user_input() -> list[str]:
     Returns: list, where each element is either a complete link or the log
     specific code.
     """
-    links = list()
-    links.append("https://www.fflogs.com/reports/a:VrNFghvTcL3J48WK")
-#    links.append("https://www.fflogs.com/reports/VwfG79rj4dF3gLqK")
-#    links.append("https://www.fflogs.com/reports/LnjBh2tfZRyv8rpD")
-    return links
+    # 1 log, kills
+    links1 = ["https://www.fflogs.com/reports/a:VrNFghvTcL3J48WK"]
+
+    # 2 logs, kills, valid comp
+    links2 = ["https://www.fflogs.com/reports/a:Byj6V2YK3NnvaJQH",
+              "https://www.fflogs.com/reports/a:rVZpJMB7cLt6xWHm"]
+
+    # 3 logs, kills, invalid comp
+    links3 = ["https://www.fflogs.com/reports/a:Byj6V2YK3NnvaJQH",
+              "https://www.fflogs.com/reports/a:rVZpJMB7cLt6xWHm",
+              "https://www.fflogs.com/reports/a:VrNFghvTcL3J48WK"]
+
+    # 2 logs, wipes, valid comp
+    links4 = ["https://www.fflogs.com/reports/a:Q8ChqfGPrVNKDjYb",
+              "https://www.fflogs.com/reports/a:hJxtKzVgXTN3rkaq"]
+
+    # 8 logs, wipes, valid comp
+    links5 = ["https://www.fflogs.com/reports/a:Q8ChqfGPrVNKDjYb",
+              "https://www.fflogs.com/reports/a:hJxtKzVgXTN3rkaq",
+              "https://www.fflogs.com/reports/a:H7rqJ6yX9x3vmaYk",
+              "https://www.fflogs.com/reports/a:HVWDad7PgZ9AzbR8",
+              "https://www.fflogs.com/reports/a:HVWDad7PgZ9AzbR8",
+              "https://www.fflogs.com/reports/a:6MjdTy4fAvkC9hz1",
+              "https://www.fflogs.com/reports/a:A7rQDhcTgR68GJf2",
+              "https://www.fflogs.com/reports/a:tPhgqTCNVcFLG6vx"]
+
+    return links5
