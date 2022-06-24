0989eef09e9ae3e5cc5006fdb6cbdc9919a44451
jakob-heller <j4kob.heller@gmail.com>
2022-06-24 18:41:53 +0200

    adjusted links to be non-anonymous

--- fflogs-study-project/input/user_input.py
+++ fflogs-study-project/input/user_input.py
@@ -8,29 +8,29 @@ def user_input() -> list[str]:
     specific code.
     """
     # 1 log, kills
-    links1 = ["https://www.fflogs.com/reports/a:VrNFghvTcL3J48WK"]
+    links1 = ["https://www.fflogs.com/reports/LnjBh2tfZRyv8rpD"]
 
     # 2 logs, kills, valid comp
-    links2 = ["https://www.fflogs.com/reports/a:Byj6V2YK3NnvaJQH",
-              "https://www.fflogs.com/reports/a:rVZpJMB7cLt6xWHm"]
+    links2 = ["https://www.fflogs.com/reports/LnjBh2tfZRyv8rpD",
+              "https://www.fflogs.com/reports/hacvwXKb8mFYrAdx"]
 
     # 3 logs, kills, invalid comp
-    links3 = ["https://www.fflogs.com/reports/a:Byj6V2YK3NnvaJQH",
-              "https://www.fflogs.com/reports/a:rVZpJMB7cLt6xWHm",
-              "https://www.fflogs.com/reports/a:VrNFghvTcL3J48WK"]
+    links3 = ["https://www.fflogs.com/reports/LnjBh2tfZRyv8rpD",
+              "https://www.fflogs.com/reports/hacvwXKb8mFYrAdx",
+              "https://www.fflogs.com/reports/vbMftnwLWzHK2prZ"]
 
     # 2 logs, wipes, valid comp
-    links4 = ["https://www.fflogs.com/reports/a:Q8ChqfGPrVNKDjYb",
-              "https://www.fflogs.com/reports/a:hJxtKzVgXTN3rkaq"]
+    links4 = ["https://www.fflogs.com/reports/waNVQPnycRWmf3r8",
+              "https://www.fflogs.com/reports/Qxdy8D9tKMcLkRvq"]
 
     # 8 logs, wipes, valid comp
-    links5 = ["https://www.fflogs.com/reports/a:Q8ChqfGPrVNKDjYb",
-              "https://www.fflogs.com/reports/a:hJxtKzVgXTN3rkaq",
-              "https://www.fflogs.com/reports/a:H7rqJ6yX9x3vmaYk",
-              "https://www.fflogs.com/reports/a:HVWDad7PgZ9AzbR8",
-              "https://www.fflogs.com/reports/a:HVWDad7PgZ9AzbR8",
-              "https://www.fflogs.com/reports/a:6MjdTy4fAvkC9hz1",
-              "https://www.fflogs.com/reports/a:A7rQDhcTgR68GJf2",
-              "https://www.fflogs.com/reports/a:tPhgqTCNVcFLG6vx"]
+    links5 = ["https://www.fflogs.com/reports/waNVQPnycRWmf3r8",
+              "https://www.fflogs.com/reports/Qxdy8D9tKMcLkRvq",
+              "https://www.fflogs.com/reports/vDYT6KryG8QHkpfd",
+              "https://www.fflogs.com/reports/qYCQyH1fhc8TgJXZ",
+              "https://www.fflogs.com/reports/ZKVArHm4GNPJTgD9",
+              "https://www.fflogs.com/reports/BnKVdNY9M7CgRDfX",
+              "https://www.fflogs.com/reports/Wm6THJ1VXDBRvpfQ",
+              "https://www.fflogs.com/reports/f4QzaWYpkr7wLJnv"]
 
-    return links5
+    return links4
