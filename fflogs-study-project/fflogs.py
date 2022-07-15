7004446fb50d5bda083d333cc6bba80c51d0351e
jakob-heller <j4kob.heller@gmail.com>
2022-07-15 10:18:27 +0200

    changed names

--- fflogs-study-project/fflogs.py
+++ fflogs-study-project/fflogs.py
@@ -8,15 +8,15 @@ from data import data_visualization as dv
 
 def main():
     """Get links from user, scrape data, combine and visualize."""
-    # input = ui.user_input()
+    inpt = ui.user_input()
 
-    # spider = ds.Scraping(input.logs, type=input.type, headless=input.headless)
-    # spider.parse_logs()
+    spider = ds.Scraping(inpt.logs, type=inpt.type, headless=inpt.headless)
+    spider.parse_logs()
 
     df_lists = dc.csv_to_dfs()
 
     dd = dc.join_dd_dfs(df_lists[0])
     hd = dc.join_hd_dfs(df_lists[1])
 
-    # dv.dash(dd, hd).run_server(debug=input.debug, use_reloader=False, port=8055)
-    dv.dash(dd, hd).run_server(debug=True, port=8056)
+    dv.dash(dd, hd).run_server(debug=inpt.debug, use_reloader=False, port=8058)
+    # dv.dash(dd, hd).run_server(debug=True, port=8056)
