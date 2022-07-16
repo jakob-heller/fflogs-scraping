f7c8164c594787b90998c63379677b38f99a73ab
jakob-heller <j4kob.heller@gmail.com>
2022-07-16 13:26:57 +0200

    extracted methods

--- src/fflogs-study-project/data/data_visualization.py
+++ src/fflogs-study-project/data/data_visualization.py
@@ -5,6 +5,79 @@ from dash import Dash, html
 from dash.dash_table import DataTable as DT
 
 
+def dash(df1: pd.DataFrame, df2: pd.DataFrame) -> Dash():
+    """Creates an interactive Dashboard with 2 sortable tables.
+
+    The style.css in the assets directory sets the dashboards
+    background color and the properties of the html.H2 object.
+
+    Args:
+      df1:
+        Pandas dataframe of summarized damage done.
+      df2:
+        Pandas dataframe of summarized healing done.
+
+    Returns:
+      Object of Dash class which can then be run on localhost.
+    """
+    app = Dash(__name__)
+    tbl1 = df_to_dt(df1, "tbl1")
+    tbl2 = df_to_dt(df2, "tbl2")
+    return layout(app, tbl1, tbl2)
+
+
+def layout(app: Dash, tbl1: DT, tbl2: DT) -> Dash():
+    """Creates the layout of the dash application."""
+    app.layout = html.Div([
+        html.H2("Damage Done"),
+        tbl1,
+        html.H2("Healing Done"),
+        tbl2,
+    ], style={"backgroundColor": "#161a1d", "padding": 40})
+    return app
+
+
+def df_to_dt(df: pd.DataFrame, id: str) -> DT:
+    """Converts dataframe into DataTable, using previously defined styles."""
+    r_column = "rDPS" if "rDPS" in df.columns else "rHPS"
+    return DT(df.to_dict("records"),
+              [{"name": i, "id": i, "selectable": True} for i in df.columns],
+              id=id,
+              sort_action="native",
+              style_as_list_view=True,
+              style_cell=styles("cell"),
+              style_header=styles("header"),
+              style_data=styles("table_data"),
+              style_data_conditional=(
+                  data_bars(df, "Amount Total") +
+                  data_bars(df, r_column) +
+                  parse_colors(df)),
+              style_cell_conditional=column_width(df)
+              )
+
+
+# The following methods are all related to the dashboard style.
+
+
+def styles(type: str) -> dict:
+    """Returns dictionary of styles as specified by "type"."""
+    if type == "header":
+        return {
+            "backgroundColor": "#0e1012",
+            "color": "white"
+        }
+    elif type == "table_data":
+        return {
+                "backgroundColor": "#161a1d",
+                "color": "white"
+        }
+    elif type == "cell":
+        return {
+            "font_size": "18px",
+            "border": "1px solid #3f3f3f"
+        }
+
+
 def data_bars(df: pd.DataFrame, column: str) -> dict:
     """Conditional formatting for data bars in cells.
 
@@ -56,6 +129,36 @@ def data_bars(df: pd.DataFrame, column: str) -> dict:
             "paddingBottom": 2,
             "paddingTop": 2
         })
+    return styles
+
+
+def column_width(df: pd.DataFrame) -> dict:
+    """Returns conditional formatting dictionary for column widths."""
+    styles = [
+        {"if": {"column_id": "Parse %"},
+         "width": "5%"},
+        {"if": {"column_id": "Player Name"},
+         "width": "10%"},
+        {"if": {"column_id": "Amount %"},
+         "width": "5%"},
+        {"if": {"column_id": "Active %"},
+         "width": "5%"},
+        {"if": {"column_id": "DPS"},
+         "width": "5%"},
+        {"if": {"column_id": "rDPS"},
+         "width": "15%"},
+        {"if": {"column_id": "HPS"},
+         "width": "5%"},
+        {"if": {"column_id": "rHPS"},
+         "width": "15%"},
+        {"if": {"column_id": "Overheal"},
+         "width": "5%"}
+    ]
+    # The healing table has one column more, We make "amount" smaller there.
+    if "HPS" in df.columns:
+        styles.append({"if": {"column_id": "Amount Total"}, "width": "45%"})
+    else:
+        styles.append({"if": {"column_id": "Amount Total"}, "width": "50%"})
     return styles
 
 
@@ -134,103 +237,3 @@ def parse_colors(df: pd.DataFrame) -> dict:
         },
     ]
     return styles
-
-
-def column_width(df: pd.DataFrame) -> dict:
-    """Returns conditional formatting dictionary for column widths."""
-    styles = [
-        {"if": {"column_id": "Parse %"},
-         "width": "5%"},
-        {"if": {"column_id": "Player Name"},
-         "width": "10%"},
-        {"if": {"column_id": "Amount %"},
-         "width": "5%"},
-        {"if": {"column_id": "Active %"},
-         "width": "5%"},
-        {"if": {"column_id": "DPS"},
-         "width": "5%"},
-        {"if": {"column_id": "rDPS"},
-         "width": "15%"},
-        {"if": {"column_id": "HPS"},
-         "width": "5%"},
-        {"if": {"column_id": "rHPS"},
-         "width": "15%"},
-        {"if": {"column_id": "Overheal"},
-         "width": "5%"}
-    ]
-    # The healing table has one column more, We make "amount" smaller there.
-    if "HPS" in df.columns:
-        styles.append({"if": {"column_id": "Amount Total"}, "width": "45%"})
-    else:
-        styles.append({"if": {"column_id": "Amount Total"}, "width": "50%"})
-    return styles
-
-
-def dash(df1: pd.DataFrame, df2: pd.DataFrame) -> Dash():
-    """Creates an interactive Dashboard with 2 sortable tables.
-
-    The style.css in the assets directory sets the dashboards
-    background color and the properties of the html.H2 object.
-
-    Args:
-      df1:
-        Pandas dataframe of summarized damage done.
-      df2:
-        Pandas dataframe of summarized healing done.
-
-    Returns:
-      Object of Dash class which can then be run on a loc.
-    """
-    app = Dash(__name__)
-
-    # Some more styles to use later
-    style_table_header = {
-        "backgroundColor": "#0e1012",
-        "color": "white"
-    }
-    style_table_data = {
-        "backgroundColor": "#161a1d",
-        "color": "white"
-    }
-    style_cell = {
-        "font_size": "18px",
-        "border": "1px solid #3f3f3f"
-    }
-    # We convert our dataframes to sortable dash datatables using the styles
-    # we defined before.
-    tbl1 = DT(df1.to_dict("records"),
-              [{"name": i, "id": i, "selectable": True} for i in df1.columns],
-              id="tbl1",
-              sort_action="native",
-              style_as_list_view=True,
-              style_cell=style_cell,
-              style_header=style_table_header,
-              style_data=style_table_data,
-              style_data_conditional=(
-                  data_bars(df1, "Amount Total") +
-                  data_bars(df1, "rDPS") +
-                  parse_colors(df1)),
-              style_cell_conditional=column_width(df1)
-              )
-    tbl2 = DT(df2.to_dict("records"),
-              [{"name": i, "id": i, "selectable": True} for i in df2.columns],
-              id="tbl2",
-              sort_action="native",
-              style_as_list_view=True,
-              style_cell=style_cell,
-              style_header=style_table_header,
-              style_data=style_table_data,
-              style_data_conditional=(
-                  data_bars(df2, "Amount Total") +
-                  data_bars(df2, "rHPS") +
-                  parse_colors(df2)),
-              style_cell_conditional=column_width(df2)
-              )
-    app.layout = html.Div([
-        html.H2("Damage Done"),
-        tbl1,
-        html.H2("Healing Done"),
-        tbl2,
-    ], style={"backgroundColor": "#161a1d", "padding": 40})
-
-    return app
